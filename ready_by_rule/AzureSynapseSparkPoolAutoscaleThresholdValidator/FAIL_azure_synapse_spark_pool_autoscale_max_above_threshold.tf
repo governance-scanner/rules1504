91a1035243d4ffe767cd267749244f905802e8da
Archana <84401974+Archanajaya-25@users.noreply.github.com>
2026-04-15 16:13:47 +0530

    Add FAIL_azure_synapse_spark_pool_autoscale_max_above_threshold.tf

---
+++ ready_by_rule/AzureSynapseSparkPoolAutoscaleThresholdValidator/FAIL_azure_synapse_spark_pool_autoscale_max_above_threshold.tf
@@ -0,0 +1,54 @@
+# Policy: AzureSynapseSparkPoolAutoscaleThresholdValidator
+# Resource type: azurerm_synapse_spark_pool
+# Checked attribute path: auto_scale.max_node_count
+# Expected: FAIL because max_node_count is above 10.
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg_fail_synapse_autoscale_max_b4" {
+  name     = "rg-fail-syn-autoscale-max-b4"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "sa_fail_synapse_autoscale_max_b4" {
+  name                     = "safailsynautosmaxb4"
+  resource_group_name      = azurerm_resource_group.rg_fail_synapse_autoscale_max_b4.name
+  location                 = azurerm_resource_group.rg_fail_synapse_autoscale_max_b4.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+}
+
+resource "azurerm_storage_data_lake_gen2_filesystem" "fs_fail_synapse_autoscale_max_b4" {
+  name               = "workspacefs"
+  storage_account_id = azurerm_storage_account.sa_fail_synapse_autoscale_max_b4.id
+}
+
+resource "azurerm_synapse_workspace" "ws_fail_synapse_autoscale_max_b4" {
+  name                                 = "synwsfailautosmaxb4"
+  resource_group_name                  = azurerm_resource_group.rg_fail_synapse_autoscale_max_b4.name
+  location                             = azurerm_resource_group.rg_fail_synapse_autoscale_max_b4.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.fs_fail_synapse_autoscale_max_b4.id
+  sql_administrator_login              = "sqladminuser"
+  sql_administrator_login_password     = "P@ssword1234!"
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_synapse_spark_pool" "fail_synapse_autoscale_max_b4" {
+  name                 = "sparkfailautosmaxb4"
+  synapse_workspace_id = azurerm_synapse_workspace.ws_fail_synapse_autoscale_max_b4.id
+  node_size_family     = "None"
+  node_size            = "Small"
+  spark_version        = "3.3"
+  cache_size           = 100
+
+  auto_scale {
+    min_node_count = 3 # ✅ Passes the min threshold check
+    max_node_count = 11 # ❌ FAIL: above the maximum threshold
+  }
+}
